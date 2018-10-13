1a99ad4d737e4e4bc320fd8a887016820b7d4812
Dennis Le <le.dennis605@gmail.com>
2018-10-13 15:50:06 -0500

    Update to barebones app, remove html elements.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,28 +1,21 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        var helloWorld = 'Welcome to the Road to learn React',
+            person = {
+                firstName: 'Dennis',
+                lastName: 'Le'
+            };
+
+        return (
+            <div className="App">
+                <h2>{helloWorld}</h2>
+                <h3>{person.firstName} {person.lastName}</h3>
+            </div>
+        );
+    }
 }
 
 export default App;
