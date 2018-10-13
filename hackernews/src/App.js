588d04175048c4e88818873f5daaf87e7647c083
Dennis Le <le.dennis605@gmail.com>
2018-10-13 16:12:57 -0500

    Add list to app.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -3,16 +3,39 @@ import './App.css';
 
 class App extends Component {
     render() {
-        const helloWorld = 'Welcome to the Road to learn React',
-            person = {
-                firstName: 'Dennis',
-                lastName: 'Le'
-            };
+        const list = [
+            {
+                title: 'React',
+                url: 'https://facebook.github.io/react/',
+                author: 'Jordan Walke',
+                num_comments: 3,
+                points: 4,
+                objectID: 0,
+            },
+            {
+                title: 'Redux',
+                url: 'https://github.com/reactjs/redux',
+                author: 'Dan Abramov, Andrew Clark',
+                num_comments: 2,
+                points: 5,
+                objectID: 1,
+            },
+        ];
 
         return (
             <div className="App">
-                <h2>{helloWorld}</h2>
-                <h3>{person.firstName} {person.lastName}</h3>
+                {list.map(function(item) {
+                    return (
+                        <div key={item.objectID}>
+                            <span>
+                                <a href={item.url}>{item.title} </a>
+                            </span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </div>
+                    );
+                })}
             </div>
         );
     }
