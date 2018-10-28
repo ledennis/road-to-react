b4b011c5c6a3acd5b53939b4289b4c0d14c75da6
Dennis Le <le.dennis605@gmail.com>
2018-10-28 18:32:58 -0500

    Refactor Search, Table, Button, from ES6 class components to functional stateless components.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -31,6 +31,7 @@ const list = [
 const isSearched = searchTerm => item =>
     item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
+// ES6 Class Components
 class App extends Component {
     constructor(props) {
         super(props);
@@ -83,73 +84,45 @@ class App extends Component {
     }
 }
 
-class Search extends Component {
-    render() {
-        const {
-            value,
-            onChange,
-            children
-        } = this.props;
-        return (
-            <form>
-                {children}
-                <input
-                    type="text"
-                    value={value}
-                    onChange={onChange}
-                />
-            </form>
-        );
-    }
-}
+// Functional Stateless component
+const Search = ({ value, onChange, children }) =>
+    <form>
+        {children}
+        <input
+            type="text"
+            value={value}
+            onChange={onChange}
+        />
+    </form>
 
-class Table extends Component {
-    render() {
-        const {
-            list,
-            pattern,
-            onDismiss
-        } = this.props;
-        return (
-            <div>
-                {list.filter(isSearched(pattern)).map(item =>
-                    <div key={item.objectID}>
-                        <span>
-                            <a href={item.url}>{item.title} </a>
-                        </span>
-                        <span>{item.author}</span>
-                        <span>{item.num_comments}</span>
-                        <span>{item.points}</span>
-                        <span>
-                            <Button onClick={() => onDismiss(item.objectID)}>
-                                Dismiss
-                            </Button>
-                        </span>
-                    </div>
-                )}
+// Functional Stateless component
+const Table = ({ list, pattern, onDismiss }) =>
+    <div>
+        {list.filter(isSearched(pattern)).map(item =>
+            <div key={item.objectID}>
+                <span>
+                    <a href={item.url}>{item.title} </a>
+                </span>
+                <span>{item.author}</span>
+                <span>{item.num_comments}</span>
+                <span>{item.points}</span>
+                <span>
+                    <Button onClick={() => onDismiss(item.objectID)}>
+                        Dismiss
+                    </Button>
+                </span>
             </div>
-        );
-    }
-}
+        )}
+    </div>
 
-class Button extends Component {
-    render() {
-        const {
-            onClick,
-            className = '',
-            children
-        } = this.props;
-
-        return (
-            <button
-                onClick={onClick}
-                className={className}
-                type="button"
-            >
-                {children}
-            </button>
-        );
-    }
-}
+// Functional Stateless component
+const Button = ({ onClick, className = '', children }) =>
+    <button
+        onClick={onClick}
+        className={className}
+        type="button"
+    >
+        {children}
+    </button>
 
 export default App;
