17df4b774b7115f4cec7096802f4762efddab5e7
Dennis Le <le.dennis605@gmail.com>
2018-10-31 18:20:38 -0500

    Refactor. Remove dummy data and isSearched function.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -12,37 +12,6 @@ const PARAM_HPP = 'hitsPerPage=';
 
 const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
-// Dummy Data
-const list = [
-    {
-        title: 'React',
-        url: 'https://facebook.github.io/react/',
-        author: 'Jordan Walke',
-        num_comments: 3,
-        points: 4,
-        objectID: 0,
-    },
-    {
-        title: 'Redux',
-        url: 'https://github.com/reactjs/redux',
-        author: 'Dan Abramov, Andrew Clark',
-        num_comments: 2,
-        points: 5,
-        objectID: 1,
-    },
-];
-
-// Without arrow functions
-// function isSearched(searchTerm) {
-//     return function(item) {
-//         return item.title.toLowerCase().includes(searchTerm.toLowerCase());
-//     }
-// }
-
-// With arrow functions
-const isSearched = searchTerm => item =>
-    item.title.toLowerCase().includes(searchTerm.toLowerCase());
-
 // ES6 Class Component
 class App extends Component {
     constructor(props) {
