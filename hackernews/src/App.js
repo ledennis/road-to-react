8f374534c862468915200a9ec2f08b657442f7ad
Dennis Le <le.dennis605@gmail.com>
2018-10-31 18:57:56 -0500

    Add workaround to prevent fetch after component unmounts.

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -15,6 +15,8 @@ const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 // ES6 Class Component
 class App extends Component {
+    _isMounted = false;
+
     constructor(props) {
         super(props);
 
@@ -71,8 +73,8 @@ class App extends Component {
 
     fetchSearchTopStories(searchTerm, page = 0) {
         axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
-            .then(result => this.setSearchTopStories(result.data))
-            .catch(error => this.setState({ error }));
+            .then(result => this._isMounted && this.setSearchTopStories(result.data))
+            .catch(error => this._isMounted && this.setState({ error }));
     }
 
     onSearchChange(event) {
@@ -116,10 +118,16 @@ class App extends Component {
     }
 
     componentDidMount() {
+        this._isMounted = true;
+
         const { searchTerm } = this.state;
 
         this.setState({ searchKey: searchTerm });
         this.fetchSearchTopStories(searchTerm);
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
     }
 
     render() {
