d0d0b381aeb2fb046d062413db538a5bcbcf0660
Dennis Le <le.dennis605@gmail.com>
2018-11-03 15:52:33 -0500

    Add Enzyme test to Button.

--- hackernews/src/components/tests/Buttons.test.js
+++ hackernews/src/components/tests/Buttons.test.js
@@ -2,12 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import { Button } from '../Buttons/Buttons';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Button', () => {
     it('renders without crashing', () => {
       const div = document.createElement('div');
       ReactDOM.render(<Button />, div);
       ReactDOM.unmountComponentAtNode(div);
+    });
+
+    it('class name is passed through', () => {
+      const element = shallow(
+          <Button className="interactions"> Button </Button>
+      )
+
+      expect(element.find('.interactions').length).toBe(1);
     });
 
     test('has a valid snapshot', () => {
