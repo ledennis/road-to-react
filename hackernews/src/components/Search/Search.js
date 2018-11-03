beca336aa70eaeb9c581e546498606888633289e
Dennis Le <le.dennis605@gmail.com>
2018-11-03 16:35:19 -0500

    Refactor Search into an ES6 class component.

--- hackernews/src/components/Search/Search.js
+++ hackernews/src/components/Search/Search.js
@@ -1,18 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-// Functional Stateless component
-export const Search = ({
-    value,
-    onChange,
-    onSubmit,
-    children }) =>
-    <form onSubmit={onSubmit}>
-        <input
-            type="text"
-            value={value}
-            onChange={onChange}
-        />
-        <button type="submit">
-            {children}
-        </button>
-    </form>
+// ES6 class component
+class Search extends Component {
+    render() {
+        const {
+            value,
+            onChange,
+            onSubmit,
+            children
+        } = this.props;
+
+        return (
+            <form onSubmit={onSubmit}>
+                <input
+                    type="text"
+                    value={value}
+                    onChange={onChange}
+                />
+                <button type="submit">
+                    {children}
+                </button>
+            </form>
+        );
+    }
+}
+
+export { Search };
